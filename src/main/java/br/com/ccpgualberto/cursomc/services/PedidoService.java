cdaa6528a1f11b13c5793bcc46ce9c58b6021255
roberto-gualberto <robertofjv26@gmail.com>
2018-03-24 19:17:53 -0400

    EndPoint /pedidos/{id} disponiveis

---
+++ src/main/java/br/com/ccpgualberto/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package br.com.ccpgualberto.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ccpgualberto.cursomc.domain.Pedido;
+import br.com.ccpgualberto.cursomc.repositories.PedidoRepository;
+import br.com.ccpgualberto.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
